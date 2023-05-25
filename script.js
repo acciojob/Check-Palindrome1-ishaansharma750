a753b6512d182f7b3f26d4aab719e872312626c9
Suansh Budhiraja <43946155+ishaansharma750@users.noreply.github.com>
2023-05-26 01:19:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 // complete the given function
 
 function palindrome(str){
+ let string = str.LowerCase();
+	string = string.trim();
+	let i=0;
+	let j= string.length-1;
 
+	while(i<j)
+		{
+			if(string[i] !== string[j])
+			{
+				return false;
+			}
+			i++;
+			j--;
+		}
+	return true;
+	
+
+	
 }
 module.exports = palindrome
