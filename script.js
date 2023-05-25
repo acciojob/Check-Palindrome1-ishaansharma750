e3a298c0b4a99fb116c260e89edb4773ecec2269
Suansh Budhiraja <43946155+ishaansharma750@users.noreply.github.com>
2023-05-26 01:24:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 // complete the given function
 
 function palindrome(str){
- let string = str.LowerCase();
-	string = string.trim();
+	let string = str.toLowerCase();
 	let i=0;
-	let j= string.length-1;
-
-	while(i<j)
-		{
-			if(string[i] !== string[j])
-			{
-				return false;
-			}
+	let j=string.length-1;
+	while(i<j){
+		if(string[i]==" "){
 			i++;
+		}
+		if(string[j]==" "){
 			j--;
 		}
+		if(string[i]!=string[j]){
+			return false;
+		}
+		i++;
+		j--;
+	}
 	return true;
-	
-
-	
 }
 module.exports = palindrome
